53b52bb53b40182306209a494ebfc213bc9254cd
Krumbholz <Henrik.Krumbholz@comparex.com>
2018-12-20 12:15:37 +0100

    New content scripts

---
+++ Content/Copy-SPFileMajorVersion.ps1
@@ -0,0 +1,127 @@
+﻿<#
+.SYNOPSIS
+    Copies a major version to a new library.
+.DESCRIPTION
+    Copies a major version to a new library.
+.NOTES
+    File Name  : Copy-SPFileMajorVersion.ps1
+    Author     : Henrik Krumbholz
+.EXAMPLE
+    Copy-SPFileMajorVersion.ps1 -WebUrl "http://test.dev.local" -SourceListName "SourceList" -TargetListName "Documents" -ItemId 12
+    Copy-SPFileMajorVersion.ps1 -WebUrl "http://test.dev.local" -SourceListName "SourceList" -TargetListName "Documents" -ItemId 12 -UseMajorVersion 7
+.PARAMETER WebUrl
+    The url of the web.
+.PARAMETER SourceListName
+    The name of the source list.
+.PARAMETER TargetListName
+    The name of the target list.
+.PARAMETER ItemId
+    The id of the item to be copied.
+.PARAMETER UseMajorVersion
+    The major version to copy if it is not the latest one.
+    Default is 0 so the latest is used.
+#>
+
+##############################
+#
+# Parameters
+#
+##############################
+
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $WebUrl,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $SourceListName,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $TargetListName,
+
+    [Parameter(Mandatory=$true)]
+    [int]
+    $ItemId,
+
+    [Parameter(Mandatory=$false)]
+    [int]
+    $UseMajorVersion = 0
+)
+
+##############################
+#
+# Snapins
+#
+##############################
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+##############################
+#
+# Main
+#
+##############################
+
+Write-Verbose "Loading web $WebUrl"
+$web = Get-SPWeb $WebUrl
+Write-Verbose "Loading source list $SourceListName"
+$sourceList = $web.Lists[$SourceListName]
+if($null -eq $sourceList)
+{
+    Write-Error "Could not find source list"
+    $web.Dispose()
+    exit
+}
+Write-Verbose "Loading item $ItemId"
+$item = $sourceList.Items.GetItemById($ItemId)
+if($null -eq $item)
+{
+    Write-Error "Could not find source item"
+    $web.Dispose()
+    exit
+}
+
+$file = $item.File
+if($null -eq $file)
+{
+    Write-Error "Could not find file"
+    $web.Dispose()
+    exit
+}
+
+if($file.MajorVersion -lt 1)
+{
+    Write-Error "No published version found for file $($file.Name)."
+    $web.Dispose()
+    exit
+}
+
+if($UseMajorVersion -lt 1 -or $UseMajorVersion -gt $file.MajorVersion)
+{
+    Write-Verbose "Major version $UseMajorVersion to low or bigger than existing major version. Version $($file.MajorVersion) used instead."
+    $UseMajorVersion = $file.MajorVersion
+}
+
+$versionLabel = "$($UseMajorVersion).0"
+Write-Verbose "Loading version $versionLabel"
+$fileVersion = $file.Versions.GetVersionFromLabel($versionLabel)
+Write-Verbose "File version $versionLabel found"
+
+$targetList = $web.Lists[$TargetListName]
+if($null -eq $targetList)
+{
+    Write-Error "Could not find target list"
+    $web.Dispose()
+    exit
+}
+
+$uploadList = $web.GetFolder($TargetListName)
+$uploadedFile = $uploadList.Files.Add($targetList.RootFolder.ServerRelativeUrl.Substring(1) + "/" + $item.Name, $fileVersion.OpenBinaryStream(), $false)
+Write-Verbose "File $($uploadedFile.ServerRelativeUrl) uploaded to web $WebUrl"
+
+$web.Dispose()
---
+++ Content/Set-SPAlternateCssUrl.ps1
@@ -0,0 +1,93 @@
+﻿<#
+.SYNOPSIS
+    Sets the alternate css url of a single web or all webs within a site collection.
+.DESCRIPTION
+    Sets the alternate css url of a single web or all webs within a site collection.
+.NOTES
+    File Name  : Set-SPAlternateCssUrl.ps1
+    Author     : Henrik Krumbholz
+.EXAMPLE
+    Set-SPAlternateCssUrl.ps1 -WebUrl "http://test.dev.local/subweb" -CssUrl "/subWeb/SiteAssets/custom.css"
+    Set-SPAlternateCssUrl.ps1 -SiteUrl "http://test.dev.local" -CssUrl "/SiteAssets/custom.css"
+.PARAMETER WebUrl
+    The url of the web to set the alternate css url.
+.PARAMETER SiteUrl
+    The url of the site collection to set the alternate css url to all webs.
+.PARAMETER CssUrl
+    The url of the CSS to set.
+#>
+
+##############################
+#
+# Parameters
+#
+##############################
+
+param(
+    [Parameter(Mandatory=$true, ParameterSetName="SingleWeb")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $WebUrl,
+
+    [Parameter(Mandatory=$true, ParameterSetName="WholeSiteCollection")]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $SiteUrl,
+
+    [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $CssUrl
+)
+
+##############################
+#
+# Snapins
+#
+##############################
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
+
+##############################
+#
+# Functions
+#
+##############################
+
+function SetCss{
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]
+        $url,
+
+        [Parameter(Mandatory=$true)]
+        [string]
+        $css
+    )
+
+    $web = Get-SPWeb $url
+    $unsafeUpates = $web.AllowUnsafeUpdates
+    $web.AllowUnsafeUpdates = $true
+    $web.AlternateCssUrl = $css
+    $web.Update()
+    $web.AllowUnsafeUpdates = $unsafeUpates
+    $web.Dispose()
+}
+
+##############################
+#
+# Main
+#
+##############################
+
+if($PSCmdlet.ParameterSetName -eq "SingleWeb")
+{
+    $site = Get-SPSite $SiteUrl
+    SetCss -url $site.RootWeb.Url -css $CssUrl
+    $site.Dispose()
+}
+
+if($PSCmdlet.ParameterSetName -eq "WholeSiteCollection")
+{
+    SetCss -url $WebUrl -css $CssUrl
+}
